ef02e2f55453cd5e7150b410cc07b4eec64aed60
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2017-01-06 13:50:40 -0200

    add desktop save

--- personal/init.el
+++ personal/init.el
@@ -5,3 +5,13 @@
 (global-linum-mode 1)
 
 
+(require 'desktop)
+;; ask to save session before closing
+(desktop-save-mode 1)
+;; save session when emacs is idle
+(defun my-desktop-save ()
+  (interactive)
+  ;; Don't call desktop-save-in-desktop-dir, as it prints a message.
+  (if (eq (desktop-owner) (emacs-pid))
+      (desktop-save desktop-dirname)))
+(add-hook 'auto-save-hook 'my-desktop-save)
