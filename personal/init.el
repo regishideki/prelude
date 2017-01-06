094ba9a9c2d3c1e2171a30e0d6ebeb3257339871
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2017-01-06 13:29:43 -0200

    fix yasnippet require, helm global option, linum option and add desktop save

---
+++ personal/init.el
@@ -0,0 +1,7 @@
+(prelude-require-packages '(yasnippet))
+
+(yas-global-mode 1)
+(helm-projectile-on)
+(global-linum-mode 1)
+
+
--- personal/preload/init.el
+++
@@ -1,5 +0,0 @@
-(prelude-require-packages '(yasnippets))
-
-(yas-global-mode 1)
-(helm-projectile-on 1)
-(global-limum-mode 1)
