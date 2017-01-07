aad64cff5804c5dedc0079512b4b13b0ef2741c0
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2017-01-07 00:54:01 -0200

    add projects to projectile

--- personal/init.el
+++ personal/init.el
@@ -5,3 +5,17 @@
 (global-linum-mode 1)
 
 (desktop-save-mode 1)
+
+(defun add-projects(dir projects)
+  (while projects
+    (projectile-add-known-project (concat dir "/" (car projects)))
+    (setq projects (cdr projects))))
+
+(defvar workspace "~/workspace")
+(defvar personal_workspace (concat workspace "/regis"))
+(defvar bankfacil_workspace (concat workspace "/bankfacil"))
+(defvar personal_projects '("programming-challenges" "presentations" "project_euler" "programming-languages"))
+(defvar bankfacil_projects '("core" "operational" "provisioning" "front" "middle-office" "ember-cli-bkf-core" "provisioning-dev" "qa-functional-specs" "docker-dev"))
+
+(add-projects personal_workspace personal_projects)
+(add-projects bankfacil_workspace bankfacil_projects)
